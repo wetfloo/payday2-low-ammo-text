893ccb92b1e7495b0087fdbd65c1df18f0174917
wetfloo <frozenyonder@gmail.com>
2026-08-19 05:26:08 +0700

    un-nest functions

--- src/lua/hooks/managers/hudmanager.lua
+++ src/lua/hooks/managers/hudmanager.lua
@@ -14,55 +14,55 @@ elseif PDTHHud or VoidUI or Holo then
 	delayed = true
 end
 
+local function apply_and_render(panel, params, name, color, offset)
+	local params_owned = {}
+
+	for k, v in pairs(params) do
+		params_owned[k] = v
+	end
+
+	if name then
+		params_owned.name = name
+	end
+	if color then
+		params_owned.color = color
+	end
+	if offset then
+		if params_owned.x and offset.x then
+			params_owned.x = params_owned.x + offset.x
+		end
+		if params_owned.y and offset.y then
+			params_owned.y = params_owned.y + offset.y
+		end
+	end
+
+	panel:text(params_owned)
+end
+
+local function shadowed_text(panel, params, text_color, shadow_color, shadow_offset)
+	local text_name = params.name
+	local shadow_name = params.name .. "_text_shadow"
+
+	-- Re-use previous text shadow instance if we simply update text.
+	-- Shadows must be rendered *before* text to not overlap.
+	local shadow_prev = panel:child(shadow_name)
+	if shadow_prev and alive(shadow_prev) then
+		shadow_prev:set_text(params.text)
+	else
+		apply_and_render(panel, params, shadow_name, shadow_color, shadow_offset)
+	end
+
+	-- Re-use previous text instance if we simply update text.
+	local text_prev = panel:child(text_name)
+	if text_prev and alive(text_prev) then
+		text_prev:set_text(params.text)
+	else
+		apply_and_render(panel, params, text_name, text_color, nil)
+	end
+end
+
 local function add_hook()
 	Hooks:PostHook(hook_class, hook_fn, "LowAmmoText_set_teammate_ammo_amount", function(_self)
-		local function shadowed_text(panel, params, text_color, shadow_color, shadow_offset)
-			local function apply_and_render(name, color, offset)
-				local params_owned = {}
-
-				for k, v in pairs(params) do
-					params_owned[k] = v
-				end
-
-				if name then
-					params_owned.name = name
-				end
-				if color then
-					params_owned.color = color
-				end
-				if offset then
-					if params_owned.x and offset.x then
-						params_owned.x = params_owned.x + offset.x
-					end
-					if params_owned.y and offset.y then
-						params_owned.y = params_owned.y + offset.y
-					end
-				end
-
-				panel:text(params_owned)
-			end
-
-			local text_name = params.name
-			local shadow_name = params.name .. "_text_shadow"
-
-			-- Re-use previous text shadow instance if we simply update text.
-			-- Shadows must be rendered *before* text to not overlap.
-			local shadow_prev = panel:child(shadow_name)
-			if shadow_prev and alive(shadow_prev) then
-				shadow_prev:set_text(params.text)
-			else
-				apply_and_render(shadow_name, shadow_color, shadow_offset)
-			end
-
-			-- Re-use previous text instance if we simply update text.
-			local text_prev = panel:child(text_name)
-			if text_prev and alive(text_prev) then
-				text_prev:set_text(params.text)
-			else
-				apply_and_render(text_name, text_color, nil)
-			end
-		end
-
 		if not managers.player:player_unit() then
 			return
 		end
