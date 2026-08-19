6350a2f847af08cc5f49c6b7cd1e42ef306f3ffb
wetfloo <frozenyonder@gmail.com>
2026-08-19 16:52:56 +0700

    cleaner text render

--- src/lua/functions/render.lua
+++ src/lua/functions/render.lua
@@ -1,7 +1,6 @@
 LowAmmoText.render = LowAmmoText.render or {}
 
-local function hud_center()
-	local hud = managers.hud:script(PlayerBase.PLAYER_INFO_HUD_FULLSCREEN_PD2)
+local function hud_center(hud)
 	local w = hud.panel:w() / 2
 	local h = hud.panel:h() / 2
 	return w, h
@@ -14,14 +13,17 @@ local function set_text_panel_size_to_rendered(panel)
 	return panel, w, h
 end
 
-LowAmmoText.render.find_or_create = function(panel, name, params_common)
+local function find_or_create(name, params_common)
+	local hud = managers.hud:script(PlayerBase.PLAYER_INFO_HUD_FULLSCREEN_PD2)
+	local panel = hud.panel
+
 	local text_name = name
 	local shadow_name = name .. "_text_shadow"
 
 	local result_text = panel:child(text_name)
 	local result_shadow = panel:child(shadow_name)
 
-	local w, h = hud_center()
+	local w, h = hud_center(hud)
 	local text_offset = LowAmmoText._data.text_offset
 	w = w + text_offset.x
 	h = h + text_offset.y
@@ -50,4 +52,30 @@ LowAmmoText.render.find_or_create = function(panel, name, params_common)
 	set_text_panel_size_to_rendered(result_shadow)
 	result_shadow:set_center(w, h)
 	result_shadow:move(1, 1)
+
+	return result_text, result_shadow
+end
+
+LowAmmoText.render.update_text = function()
+	if not managers.player:player_unit() then
+		return
+	end
+
+	local equipped_unit = managers.player:player_unit():movement():current_state()._equipped_unit
+	if
+		not equipped_unit
+		or not alive(equipped_unit)
+		or equipped_unit ~= managers.player:equipped_weapon_unit()
+	then
+		return
+	end
+
+	local current_left = equipped_unit:base():get_ammo_total()
+	local current_clip = equipped_unit:base():get_ammo_remaining_in_clip()
+
+	return find_or_create("low_ammo_text__ammo_indicator", {
+		text = string.format("%d :: %d", current_clip, current_left),
+		font = tweak_data.menu.pd2_medium_font,
+		font_size = 16,
+	})
 end
--- src/lua/hooks/managers/menumanager.lua
+++ src/lua/hooks/managers/menumanager.lua
@@ -5,30 +5,16 @@ Hooks:Add(
 		MenuHelper:LoadFromJsonFile(
 			LowAmmoText.mod_path .. "menus/blt_options.json",
 			LowAmmoText,
-			LowAmmoText._data
+			-- TODO: setting it like this is bad,
+			-- because we don't have a way to set other values.
+			LowAmmoText._data.text_offset
 		)
 
 		-- Add our own callbacks to handle menu value changes
 		MenuCallbackHandler.low_ammo_text__menu_callback__indicator_offset_y = function(_self, item)
-			local value = item:value()
-			local hud = managers.hud:script(PlayerBase.PLAYER_INFO_HUD_FULLSCREEN_PD2)
+			LowAmmoText._data.text_offset.y = item
 
-			LowAmmoText._data.offset_y = value
-
-			local text = LowAmmoText._rendered_indicators.text
-			local shadow = LowAmmoText._rendered_indicators.shadow
-
-			local text_x, text_y = text:center()
-			local shadow_x, shadow_y = shadow:center()
-			local shadow_offset_x = shadow_x - text_x
-			local shadow_offset_y = shadow_y - text_y
-
-			text_y = (hud.panel:w() / 2) + value
-			shadow_x = shadow_x + shadow_offset_x
-			shadow_y = text_y + shadow_offset_y
-
-			text:set_center(text_x, text_y)
-			shadow:set_center(shadow_x, shadow_y)
+			LowAmmoText.render.update_text()
 		end
 
 		MenuCallbackHandler.low_ammo_text__menu_callback__closed = function(_self) end
