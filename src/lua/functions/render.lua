5743b3f6f169fa9a7c80b19d4b3b817e47d6788e
wetfloo <frozenyonder@gmail.com>
2026-08-19 21:04:27 +0700

    less reliant on params, add logging

--- src/lua/functions/render.lua
+++ src/lua/functions/render.lua
@@ -1,5 +1,8 @@
 LowAmmoText.render = LowAmmoText.render or {}
 
+LowAmmoText.render.text_name = "low_ammo_text__ammo_indicator"
+LowAmmoText.render.shadow_name = LowAmmoText.render.text_name .. "__text_shadow"
+
 local function hud_center(hud)
 	local w = hud.panel:w() / 2
 	local h = hud.panel:h() / 2
@@ -13,15 +16,12 @@ local function set_text_panel_size_to_rendered(panel)
 	return panel, w, h
 end
 
-local function find_or_create(name, params_common)
+local function find_or_create(params_common)
 	local hud = managers.hud:script(PlayerBase.PLAYER_INFO_HUD_FULLSCREEN_PD2)
 	local panel = hud.panel
 
-	local text_name = name
-	local shadow_name = name .. "_text_shadow"
-
-	local result_text = panel:child(text_name)
-	local result_shadow = panel:child(shadow_name)
+	local result_text = panel:child(LowAmmoText.render.text_name)
+	local result_shadow = panel:child(LowAmmoText.render.shadow_name)
 
 	local w, h = hud_center(hud)
 	local text_offset = LowAmmoText._data.text_offset
@@ -30,7 +30,7 @@ local function find_or_create(name, params_common)
 
 	if not result_shadow or not alive(result_shadow) then
 		local params = LowAmmoText.tbl.shallow_copy(params_common)
-		params.name = name .. "_text_shadow"
+		params.name = LowAmmoText.render.shadow_name
 		params.color = Color.black
 
 		result_shadow = panel:text(params)
@@ -40,7 +40,7 @@ local function find_or_create(name, params_common)
 
 	if not result_text or not alive(result_text) then
 		local params = LowAmmoText.tbl.shallow_copy(params_common)
-		params.name = name
+		params.name = LowAmmoText.render.text_name
 		params.color = Color.white
 
 		result_text = panel:text(params)
@@ -50,12 +50,14 @@ local function find_or_create(name, params_common)
 
 	-- Updating text could cause the text size to change,
 	-- so we handle that by re-centering and moving each time.
-	set_text_panel_size_to_rendered(result_text)
-	result_text:set_center(w, h)
-
 	set_text_panel_size_to_rendered(result_shadow)
 	result_shadow:set_center(w, h)
 	result_shadow:move(1, 1)
+
+	set_text_panel_size_to_rendered(result_text)
+	result_text:set_center(w, h)
+
+	Utils.PrintTable({ x = result_text:center_x(), y = result_text:center_y() })
 
 	return result_text, result_shadow
 end
@@ -77,7 +79,7 @@ LowAmmoText.render.update_text = function()
 	local current_left = equipped_unit:base():get_ammo_total()
 	local current_clip = equipped_unit:base():get_ammo_remaining_in_clip()
 
-	return find_or_create("low_ammo_text__ammo_indicator", {
+	return find_or_create({
 		text = string.format("%d :: %d", current_clip, current_left),
 		font = tweak_data.menu.pd2_medium_font,
 		font_size = 16,
