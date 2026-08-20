b823486db3ba32a6b240aed21e18de74922e0f1a
wetfloo <frozenyonder@gmail.com>
2026-08-21 00:03:00 +0700

    add destroy hook, reformat

--- src/lua/hooks/managers/hudmanager.lua
+++ src/lua/hooks/managers/hudmanager.lua
@@ -15,46 +15,63 @@ elseif PDTHHud or VoidUI or Holo then
 end
 
 local function init_hooks()
-	Hooks:PostHook(hook_class, hook_fn, "LowAmmoText_set_teammate_ammo_amount", function(_self)
-		if not managers.player:player_unit() then
-			return
-		end
+	Hooks:PostHook(
+		hook_class,
+		hook_fn,
+		"LowAmmoText_set_teammate_ammo_amount_alive",
+		function(_self)
+			if not managers.player:player_unit() then
+				return
+			end
 
-		local equipped_unit =
-			managers.player:player_unit():movement():current_state()._equipped_unit
-		if
-			not equipped_unit
-			or not alive(equipped_unit)
-			or equipped_unit ~= managers.player:equipped_weapon_unit()
-		then
-			return
-		end
+			local equipped_unit =
+				managers.player:player_unit():movement():current_state()._equipped_unit
+			if
+				not equipped_unit
+				or not alive(equipped_unit)
+				or equipped_unit ~= managers.player:equipped_weapon_unit()
+			then
+				return
+			end
 
-		local current_left = equipped_unit:base():get_ammo_total()
-		local current_clip = equipped_unit:base():get_ammo_remaining_in_clip()
-		local s = string.format("%s :: %s", current_clip, current_left)
+			local current_left = equipped_unit:base():get_ammo_total()
+			local current_clip = equipped_unit:base():get_ammo_remaining_in_clip()
+			local s = string.format("%s :: %s", current_clip, current_left)
 
-		if not LowAmmoText.rendered_text then
-			LowAmmoText.rendered_text = LowAmmoText.RenderedText:new({
-				s = s,
-				text_configuration = {
-					color = Color.white,
-					offset = LowAmmoText._data.text_offset,
-					font_size = 16,
-					font = tweak_data.menu.pd2_medium_font,
-				},
-				text_shadow_configuration = {
-					color = Color.black,
-					additional_offset = {
-						x = 1,
-						y = 1,
+			if not LowAmmoText.rendered_text then
+				LowAmmoText.rendered_text = LowAmmoText.RenderedText:new({
+					s = s,
+					text_configuration = {
+						color = Color.white,
+						offset = LowAmmoText._data.text_offset,
+						font_size = 16,
+						font = tweak_data.menu.pd2_medium_font,
 					},
-				},
-			})
-		else
-			LowAmmoText.rendered_text:set_s(s)
+					text_shadow_configuration = {
+						color = Color.black,
+						additional_offset = {
+							x = 1,
+							y = 1,
+						},
+					},
+				})
+			else
+				LowAmmoText.rendered_text:set_s(s)
+			end
 		end
-	end)
+	)
+
+	Hooks:PostHook(
+		hook_class,
+		"destroy",
+		"LowAmmoText_set_teammate_ammo_amount_destroy",
+		function(_self)
+			if LowAmmoText.rendered_text then
+				LowAmmoText.rendered_text:destroy()
+				LowAmmoText.rendered_text = nil
+			end
+		end
+	)
 end
 
 if delayed then
