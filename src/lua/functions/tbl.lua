e9425010d71f6b74319825500ab913807f1bf824
wetfloo <frozenyonder@gmail.com>
2026-08-21 23:14:21 +0700

    add tbl util

--- src/lua/functions/tbl.lua
+++ src/lua/functions/tbl.lua
@@ -9,3 +9,29 @@ function LowAmmoText.tbl.shallow_copy(tbl)
 
 	return result
 end
+
+---@param target table
+---@param filler table
+---@return table
+local function fill_missing_rec(target, filler)
+	for k, v in pairs(filler) do
+		if not target[k] then
+			-- No need to worry about inner table structure,
+			-- as `filler` should have the entire table
+			-- structure for `target`'s missing fields.
+			-- If it doesn't, too bad.
+			target[k] = filler
+		elseif type(target[k]) == "table" and type(v) == "table" then
+			fill_missing_rec(target[k], v)
+		end
+	end
+
+	return target
+end
+
+---@param target table
+---@param filler table
+---@return table
+function LowAmmoText.tbl.fill_missing(target, filler)
+	return fill_missing_rec(target, filler)
+end
