428c268875769d0237bf3e19f9601d2286b3e807
wetfloo <frozenyonder@gmail.com>
2026-08-25 23:05:16 +0700

    replace LowAmmoText strings w/ table const

--- src/lua/hooks/lib/states/ingamewaitingforrespawn.lua
+++ src/lua/hooks/lib/states/ingamewaitingforrespawn.lua
@@ -1,4 +1,4 @@
-Hooks:PostHook(IngameWaitingForRespawnState, "at_enter", "LowAmmoText__posthook__custody_enter_handler", function (_self)
+Hooks:PostHook(IngameWaitingForRespawnState, "at_enter", LowAmmoText.mod_name .. "__posthook__custody_enter_handler", function (_self)
 	if LowAmmoText.ammo_state_manager then
 		LowAmmoText.ammo_state_manager:destroy()
 		LowAmmoText.ammo_state_manager = nil
