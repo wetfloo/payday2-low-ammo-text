1ff77223b2fdee2c01c63903042cd350b7d24042
wetfloo <frozenyonder@gmail.com>
2026-08-27 22:51:00 +0700

    initial anim implementation

--- src/lua/classes/ammo_state_manager.lua
+++ src/lua/classes/ammo_state_manager.lua
@@ -115,4 +115,18 @@ function LowAmmoText.AmmoStateManager:_on_state_value_update()
 	self._rendered_text:set_s(managers.localization:text(preset.s_id))
 	self._rendered_text:set_text_color(preset.color)
 	self._rendered_text:show(LowAmmoText._data.text_fade_duration_secs)
+
+	-- TODO: don't break into private variables
+	self._rendered_text._text:animate(function(o)
+		while true do
+			local r, g, b = o:color()
+			local h, s, v = rgb_to_hsv(r, g, b)
+			v = math.lerp(0.5, 1.0, math.sin(Application:time()))
+
+			r, g, b = hsv_to_rgb(h, s, v)
+			o:set_text_color(r, g, b)
+
+			coroutine.yield()
+		end
+	end)
 end
