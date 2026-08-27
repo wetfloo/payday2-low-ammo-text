232eb32a5319d313fdc2377205c1ca92d7dd1a42
wetfloo <frozenyonder@gmail.com>
2026-08-28 00:35:49 +0700

    use the new add_text_indicator to animate

--- src/lua/classes/ammo_state_manager.lua
+++ src/lua/classes/ammo_state_manager.lua
@@ -56,24 +56,19 @@ function LowAmmoText.AmmoStateManager:init(rendered_text)
 
 	self._rendered_text = rendered_text
 
-	-- TODO: don't break into private variables
-	rendered_text._text:animate(function(o)
-		while true do
-			local t = Application:time()
-			local t_mul_base = 100
-			-- TODO: make this parameter configurable by the user
-			local t_mul_param = 4
+	self._rendered_text:add_text_animator("ammo_state_manager_text_animator", function (o)
+		local t = Application:time()
+		local t_mul_base = 100
+		-- TODO: make this parameter configurable by the user
+		local t_mul_param = 4
 
-			local color = o:color()
+		local color = o:color()
 
-			local h, s, v = rgb_to_hsv(color.r, color.g, color.b)
-			v = math.lerp(0.5, 1.0, (math.sin(t * t_mul_base * t_mul_param) + 1) / 2)
-			local r, g, b = hsv_to_rgb(h, s, v)
+		local h, s, v = rgb_to_hsv(color.r, color.g, color.b)
+		v = math.lerp(0.5, 1.0, (math.sin(t * t_mul_base * t_mul_param) + 1) / 2)
+		local r, g, b = hsv_to_rgb(h, s, v)
 
-			o:set_color(Color(r, g, b))
-
-			coroutine.yield()
-		end
+		o:set_color(Color(r, g, b))
 	end)
 end
 
