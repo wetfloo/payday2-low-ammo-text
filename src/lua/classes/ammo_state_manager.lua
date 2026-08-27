d5af5482222093a08189050def9020a0e41a870e
wetfloo <frozenyonder@gmail.com>
2026-08-28 00:04:24 +0700

    update the animation function with params

--- src/lua/classes/ammo_state_manager.lua
+++ src/lua/classes/ammo_state_manager.lua
@@ -60,11 +60,14 @@ function LowAmmoText.AmmoStateManager:init(rendered_text)
 	rendered_text._text:animate(function(o)
 		while true do
 			local t = Application:time()
+			local t_mul_base = 100
+			-- TODO: make this parameter configurable by the user
+			local t_mul_param = 4
 
 			local color = o:color()
 
 			local h, s, v = rgb_to_hsv(color.r, color.g, color.b)
-			v = math.lerp(0.5, 1.0, (math.sin(t * 400) + 1) / 2)
+			v = math.lerp(0.5, 1.0, (math.sin(t * t_mul_base * t_mul_param) + 1) / 2)
 			local r, g, b = hsv_to_rgb(h, s, v)
 
 			o:set_color(Color(r, g, b))
