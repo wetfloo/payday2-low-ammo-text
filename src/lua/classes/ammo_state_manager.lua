4196d5e440e8d51a46c5a83a9e560af2b316d687
wetfloo <frozenyonder@gmail.com>
2026-08-28 19:49:22 +0700

    remove bad looking functions

--- src/lua/classes/ammo_state_manager.lua
+++ src/lua/classes/ammo_state_manager.lua
@@ -78,7 +78,7 @@ function LowAmmoText.AmmoStateManager:init(rendered_text)
 		local time = Application:time()
 		local base_time_mul = 50
 		local product = time * base_time_mul * LowAmmoText._data.pulse_text_animation_speed_mul
-		local sine_norm = (math.sin(product) + 1) / 2
+		local sine_norm = (math.cos(product) + 1) / 2
 		local eased = LowAmmoText._data.pulse_text_ease_function(sine_norm)
 		local mul = math.lerp(LowAmmoText._data.pulse_text_animation_start, 1.0, eased)
 
