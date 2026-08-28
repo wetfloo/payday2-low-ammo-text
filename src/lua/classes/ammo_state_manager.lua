95f6878d85a40537b9b18dbf7a419878f64676f9
wetfloo <frozenyonder@gmail.com>
2026-08-28 17:11:07 +0700

    add a custom easing function use

--- src/lua/classes/ammo_state_manager.lua
+++ src/lua/classes/ammo_state_manager.lua
@@ -79,7 +79,8 @@ function LowAmmoText.AmmoStateManager:init(rendered_text)
 		local base_time_mul = 50
 		local product = time * base_time_mul * LowAmmoText._data.pulse_text_animation_speed_mul
 		local sine_norm = (math.sin(product) + 1) / 2
-		local mul = math.lerp(LowAmmoText._data.pulse_text_animation_start, 1.0, sine_norm)
+		local eased = LowAmmoText.ease.in_out_circ(sine_norm)
+		local mul = math.lerp(LowAmmoText._data.pulse_text_animation_start, 1.0, eased)
 
 		local r, g, b = preset_color.r, preset_color.g, preset_color.b
 		local h, s, v = rgb_to_hsv(r, g, b)
